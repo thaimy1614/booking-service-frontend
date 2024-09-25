24b356c5ff66ff6569dc09cffec686f4a3715aad
quocthai <lxlthailxl@gmail.com>
2024-09-25 22:01:59 +0700

    add: authenticate when login with google

---
+++ src/components/common/Authenticate.jsx
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { setToken } from "../services/localStorageService";
+import { Box, CircularProgress, Typography } from "@mui/material";
+
+export default function Authenticate() {
+  const navigate = useNavigate();
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  useEffect(() => {
+    console.log(window.location.href);
+
+    const authCodeRegex = /code=([^&]+)/;
+    const isMatch = window.location.href.match(authCodeRegex);
+
+    if (isMatch) {
+      const authCode = isMatch[1];
+
+      fetch(
+        `http://localhost:8080/identity/auth/outbound/authentication?code=${authCode}`,
+        {
+          method: "POST",
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+
+          setToken(data.result?.token);
+          setIsLoggedin(true);
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedin) {
+      navigate("/");
+    }
+  }, [isLoggedin, navigate]);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress></CircularProgress>
+        <Typography>Authenticating...</Typography>
+      </Box>
+    </>
+  );
+}
