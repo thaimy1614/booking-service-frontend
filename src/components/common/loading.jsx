ed6894ca6933d9d5894a305b181bb1f410c6ab6b
quocthai <lxlthailxl@gmail.com>
2024-10-01 17:02:38 +0700

    add loading component

---
+++ src/components/common/loading.jsx
@@ -0,0 +1,22 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
+import React from "react";
+
+const Loading = () => {
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress></CircularProgress>
+        <Typography>Authenticating...</Typography>
+      </Box>
+    </>
+}
+
+export default Loading;
