e867f3950b9bdd9fa406aafbcf9adaaee7f2fc53
quocthai <lxlthailxl@gmail.com>
2024-10-22 19:18:05 +0700

    implement send contact request at footer

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -1,23 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./footer.css";
+import { SendRequest } from "../request";
+import MessageModal from "../message-modal";
 
 export const Footer = () => {
+  const [messageModalOpen, setMessageModal] = useState(false);
+  const [messageType, setMessageType] = useState(false);
+  const [message, setMessage] = useState(
+    "Gửi yêu cầu tư vấn thành công, vui lòng kiểm tra email"
+  );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleMessageModalClose = () => {
+    setMessageModal(false);
+  };
   return (
     <footer className="footer">
+      <MessageModal
+        message={message}
+        open={messageModalOpen}
+        handleClose={handleMessageModalClose}
+        messageType={messageType}
+      />
       <div className="footer-subscribe">
         <h3>Đăng ký để nhận nội dung mới nhất từ ​​S-Service</h3>
-        <input type="text" placeholder="Tên" />
-        <input type="email" placeholder="Email" />
-        <button className="footer-btn">Tư vấn ngay</button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            SendRequest(
+              name,
+              email,
+              setMessageModal,
+              setMessageType,
+              setMessage
+            );
+          }}
+        >
+          <input
+            required
+            type="text"
+            placeholder="Tên"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            required
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button button="submit" className="footer-btn">
+            Tư vấn ngay
+          </button>
+        </form>
       </div>
       <div className="footer-columns">
         <div className="footer-column">
-        <h2>Kết nối tới S-Service</h2>
+          <h2>Kết nối tới S-Service</h2>
           <div className="social-icons">
             <br></br>
-            <a href="https://www.facebook.com/profile.php?id=61566307315716" target="_blank" rel="noreferrer noopener"><img src="/assets/img/facebook.png" alt="Facebook" /></a>
-            <a href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=sservice.ia@gmail.com&tf=1" target="_blank" rel="noreferrer noopener"><img src="/assets/img/gmail.png" alt="Gmail" /></a>
-            <a href="https://www.instagram.com/sservice.it?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noreferrer noopener"><img src="/assets/img/instagram.png" alt="Instagram" /></a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61566307315716"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src="/assets/img/facebook.png" alt="Facebook" />
+            </a>
+            <a
+              href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=sservice.ia@gmail.com&tf=1"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src="/assets/img/gmail.png" alt="Gmail" />
+            </a>
+            <a
+              href="https://www.instagram.com/sservice.it?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img src="/assets/img/instagram.png" alt="Instagram" />
+            </a>
           </div>
         </div>
         <div className="footer-column">
